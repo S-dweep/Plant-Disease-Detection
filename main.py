02294b34e8b1a9ca2f6174001aa7fc867444403a
Sayandeep Mondal <mondalsayandeep2004@gmail.com>
2025-07-10 16:31:57 +0530

    commit 2

--- main.py
+++ main.py
@@ -1,45 +1,110 @@
-#Library imports
-import os
-import numpy as np
 import streamlit as st
-import cv2
-from keras.models import load_model
+import tensorflow as tf
+import numpy as np
 
+#Tensorflow Model Prediction
+def model_prediction(test_image):
+    model  = tf.keras.models.load_model('trained_model.keras')
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128, 128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #Convert single image to a batch
+    prediction = model.predict(input_arr)
+    result_index = np.argmax(prediction)
+    return result_index
 
-#Loading the Model
-model = load_model('C:/Users/Sayandeep Mondal/solo_pjcts/leaf_disease_detection/trained_prediction_model.keras')
+#Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
 
-#Name of Classes
-data_dir = 'C:/Users/Sayandeep Mondal/solo_pjcts/leaf_disease_detection/train'
-CLASS_NAMES = os.listdir(data_dir)
+#Home Page
+if(app_mode=="Home"):
+    st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    image_path = "home_page.jpeg"
+    st.image(image_path,use_column_width=True)
+    st.markdown("""
+    Welcome to the Plant Disease Recognition System! 🌿🔍
+    
+    Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
 
-#Setting Title of App
-st.title("Plant Disease Detection")
-st.markdown("Upload an image of the plant leaf")
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
 
-#Uploading the dog image
-plant_image = st.file_uploader("Choose an image...", type="jpg")
-submit = st.button('Predict')
-#On predict button click
-if submit:
+    ### Why Choose Us?
+    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
+    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
+    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
 
+    ### Get Started
+    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Plant Disease Recognition System!
 
-    if plant_image is not None:
+    ### About Us
+    Learn more about the project, our team, and our goals on the **About** page.
+""")
 
-        # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-
-
-
-        # Displaying the image
-        st.image(opencv_image, channels="BGR")
-        st.write(opencv_image.shape)
-        #Resizing the image
-        opencv_image = cv2.resize(opencv_image, (256,256))
-        #Convert image to 4 Dimension
-        opencv_image.shape = (1,256,256,3)
-        #Make Prediction
-        Y_pred = model.predict(opencv_image)
-        result = CLASS_NAMES[np.argmax(Y_pred)]
-        st.title(str("This is "+result.split('___')[0]+ " leaf with " + result.split('___')[1]))
+#About Page
+elif(app_mode=="About"):
+    st.header("About")
+    st.markdown("""
+    #### About Dataset
+    This dataset is recreated using offline augmentation from the original dataset. The original dataset can be found on this github repo. This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes. The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure. A new directory containing 33 test images is created later for prediction purpose.
+    #### Content
+    1. Train (70295 images)
+    2. Valid (17572 image)
+    3. Test (33 images)
+""")
+    
+#Prediction Page
+elif(app_mode=="Disease Recognition"):
+    st.header("Disease Recognition")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,use_column_width=True)
+    #Predict Button
+    if(st.button("Predict")):
+        with st.spinner("Please Wait.."):
+            st.write("Our Prediction")
+            result_index = model_prediction(test_image)
+            #Define Class
+            class_name = [
+            'Apple___Apple_scab',
+            'Apple___Black_rot',
+            'Apple___Cedar_apple_rust',
+            'Apple___healthy',
+            'Blueberry___healthy',
+            'Cherry_(including_sour)___Powdery_mildew',
+            'Cherry_(including_sour)___healthy',
+            'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+            'Corn_(maize)___Common_rust_',
+            'Corn_(maize)___Northern_Leaf_Blight',
+            'Corn_(maize)___healthy',
+            'Grape___Black_rot',
+            'Grape___Esca_(Black_Measles)',
+            'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+            'Grape___healthy',
+            'Orange___Haunglongbing_(Citrus_greening)',
+            'Peach___Bacterial_spot',
+            'Peach___healthy',
+            'Pepper,_bell___Bacterial_spot',
+            'Pepper,_bell___healthy',
+            'Potato___Early_blight',
+            'Potato___Late_blight',
+            'Potato___healthy',
+            'Raspberry___healthy',
+            'Soybean___healthy',
+            'Squash___Powdery_mildew',
+            'Strawberry___Leaf_scorch',
+            'Strawberry___healthy',
+            'Tomato___Bacterial_spot',
+            'Tomato___Early_blight',
+            'Tomato___Late_blight',
+            'Tomato___Leaf_Mold',
+            'Tomato___Septoria_leaf_spot',
+            'Tomato___Spider_mites Two-spotted_spider_mite',
+            'Tomato___Target_Spot',
+            'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+            'Tomato___Tomato_mosaic_virus',
+            'Tomato___healthy'
+            ]
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
